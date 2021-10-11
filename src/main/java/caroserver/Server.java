a51833bfda8c6b5889a125db5fde610e0404d645
Zagon102 <sonvu11022k@gmail.com>
2021-10-11 17:35:34 +0700

    version2.2 add login and create

--- src/main/java/caroserver/Server.java
+++ src/main/java/caroserver/Server.java
@@ -10,6 +10,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 
 import utils.Value;
 
@@ -18,6 +19,15 @@ import utils.Value;
  * @author Son Vu
  */
 public class Server {
+	private ArrayList<Player> playerList;
+	
+	public Server() {
+		playerList = new ArrayList<Player>();
+		//TEST BEFORE ADD DATABASE
+		playerList.add(new Player("player1"));
+		playerList.add(new Player("player2"));
+		playerList.add(new Player("player3"));
+	}
 	
 	public void start() {
 		try {
@@ -30,11 +40,11 @@ public class Server {
 				byte[] buf = new byte[1024];
 				DatagramPacket p = new DatagramPacket(buf,buf.length);
 				server.receive(p);
-				String msg = new String(buf).trim();
+				String msg = (new String(buf)).trim();
 				System.out.print("$ Msg received : [" + msg + "]");
 				System.out.println(" from : " + p.getAddress() + ":" + p.getPort());
 				String responseMsg = analyzer.analyze(msg);
-				System.out.println("$ Msg responsed :  " + responseMsg);
+				System.out.println("$ Msg responsed :  [" + responseMsg + "]");
 				p = new DatagramPacket(responseMsg.getBytes(), responseMsg.length(), InetAddress.getByName(Value.groupAddress), Value.clientPort);
 				server.send(p);
 				
@@ -47,22 +57,40 @@ public class Server {
 	private class MessageAnalyzer {	
 		
 		public String analyze(String msg) {
-			String result = "none";
+			StringBuilder result = new StringBuilder("");
 			String[] t = msg.split(" ");
 			// Request : [connect] - Response : [accept connection] (Connection accepted, create new ClientSession)
 			if(t[0].equalsIgnoreCase("connect")) {
-				result = "accept connection";
-			}
-			// Request : [login <username> <password>] - Response : [accept <username>] (Login accepted, initiate client information)
-			else if(t[0].equalsIgnoreCase("login")) {
-				String username = t[1];
+				result.append("accept connection");
+			} // Request : [ <username> <....>] - Response : [<username> <....>] (Authorized username for sending message)
+			
+			// Request : [<username> login <password>] - Response : [<username> login accept] (Login accepted, initiate client information)
+			else if(t[1].equalsIgnoreCase("login")) {
+				String username = t[0];
 				String password = t[2];
+				
 				//TEST BEFORE ADD DATABASE
 				if(username.equalsIgnoreCase(password)) {
-					result = "accept " + username;
+					result.append(username + " login accept");
+				} else {
+					result.append(username + " login deny");
 				}
+				
+			} 
+			// Request : [ <username> refresh] - Response : [<username> refresh <username1> <username2> ...] (Sending Player list)
+			else if(t[1].equalsIgnoreCase("refresh")) {
+				result.append(t[0] + " refresh");
+				for(Player i : playerList) {
+					result.append(" " + i.getUsername());
+				}
+				
+			} 
+			// Request : [ <username> create] - Response : [<username> create accept] (Create room for <username>)
+			else if(t[1].equalsIgnoreCase("create")) {
+				playerList.add(new Player(t[0]));
+				result.append(t[0] + " create accept");
 			}
-			return result;
+			return result.toString();
 		}
 		
 	}
